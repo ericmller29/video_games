aaf441893a619d90a19f3598b00bbe467d6ebd7e
Eric <emiller@tax-guard.com>
2017-09-14 12:38:09 -0600

    Added caching to the search

--- server.js
+++ server.js
@@ -2,7 +2,27 @@ var express = require('express'),
 	app = express(),
 	exec = require('child_process').exec,
     request = require('request'),
-    api_key = 'a96cf8a79064fbf8fe36cb18dd7af46eb689f02b';
+    api_key = 'a96cf8a79064fbf8fe36cb18dd7af46eb689f02b',
+    mcache = require('memory-cache');
+
+var cache = function(duration){
+    return function(req, res, next){
+        var key = '__express__' + req.originalUrl || req.url;
+        var cachedBody = mcache.get(key);
+
+        if(cachedBody){
+            res.send(cachedBody);
+            return;
+        }else{
+            res.sendResponse = res.send;
+            res.send = function(body){
+                mcache.put(key, body, duration * 100000);
+                res.sendResponse(body);
+            }
+            next();
+        }
+    }
+}
 
 app.use(function(req, res, next){    
     res.header("Access-Control-Allow-Origin", "http://games.eastoh.co");
@@ -15,7 +35,8 @@ app.use(function(req, res, next){
 app.get('/scraper/', function(req, res){
     res.send('Scrape it!');
 });
-app.get('/scraper/search', function(req, res){
+app.get('/scraper/search', cache(864), function(req, res){
+            console.log('should not make it here');
     var requestOptions = {
         url: 'https://www.giantbomb.com/api/games',
         headers: {
@@ -42,7 +63,7 @@ app.get('/scraper/search', function(req, res){
     //     res.json(response);
     // });
 });
-app.get('/scraper/bestbuy', function(req, res){
+app.get('/scraper/bestbuy', cache(864), function(req, res){
 	res.setHeader('Content-Type', 'application/json');
 	console.log(req.query.game);
     exec('casperjs bestbuy.js --game="' + req.query.game + '"', function(err, results){
@@ -53,7 +74,7 @@ app.get('/scraper/bestbuy', function(req, res){
     	})
     });
 });
-app.get('/scraper/gamestop', function(req, res){
+app.get('/scraper/gamestop', cache(864), function(req, res){
 	res.setHeader('Content-Type', 'application/json');
     exec('python3 gamestop.py "' + req.query.game + '"', function(err, results){
     	res.json({
